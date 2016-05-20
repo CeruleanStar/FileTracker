245f588b6ddedb7b8e3c0b7145de52d747ebf5e2
CeruleanStar <stephen.kasler@gmail.om>
2016-05-20 10:26:33 -0500

    changed a label to a link label so they can click on it to open up explorer ro the directory path
    added clickable event for the notification popup to also open up explorer to the network location that generated the event

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -30,21 +30,16 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(111, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Lcation: \\\\fs3\\it\\other";
             // 
             // label2
             // 
@@ -77,18 +72,75 @@
             // 
             this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
-            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Text = "FileWatcher";
             this.notifyIcon1.Visible = true;
+            this.notifyIcon1.BalloonTipClicked += new System.EventHandler(this.notifyIcon1_BalloonTipClicked);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Location \\\\fs3\\it\\other\\test";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 86);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Has";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(50, 86);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(70, 86);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Current Files";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.ForeColor = System.Drawing.Color.Green;
+            this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.linkLabel1.Location = new System.Drawing.Point(18, 13);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(63, 13);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Registration";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(204, 416);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -97,12 +149,15 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -12,59 +12,118 @@ using System.Windows;
 using System.Windows.Threading;
 
 
+
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        public const string dirPath1 = "\\\\tchospital\\files\\faxes\\registration";
+        public const string dirPath2 = "\\\\tchospital\\files\\faxes\\registration\\to be scheduled";
+        public const string dirPath3 = "[directory path 3]";
+
+        public string ballonDir = dirPath1;
+
         public Form1()
         {
             InitializeComponent();
-            
+
             notifyIcon1.BalloonTipText = "File Watcher Started";
             notifyIcon1.ShowBalloonTip(1000);
 
             watch1();
-            this.label2.Text = fileCount1().ToString();
- 
+            watch2();
+            // watch3();
+
+            this.linkLabel1.Text = dirPath1;
+            this.label5.Text = dirPath2;
+            
+            // type a comment real quick there
+            this.label2.Text = fileCount1(dirPath1).ToString();
+            this.label7.Text = fileCount1(dirPath2).ToString();
+
         }
 
 
         public void watch1()
         {
             FileSystemWatcher fsWatch = new FileSystemWatcher();
-            fsWatch.Path = "[Folder Location]";
+            fsWatch.Path = dirPath1;
             fsWatch.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
             fsWatch.Filter = "*.*";
-            fsWatch.Created += new FileSystemEventHandler(onCreated);
+            fsWatch.Created += new FileSystemEventHandler((sender, e) => onCreated(sender, e, dirPath1));
             fsWatch.Deleted += new FileSystemEventHandler(onDeleted);
             fsWatch.EnableRaisingEvents = true;
         }
 
-
-        public void onCreated(object source, FileSystemEventArgs e)
+        public void watch2()
         {
-            FlashWindow.Flash(this);
-            this.label2.Text = fileCount1().ToString();
-            notifyIcon1.BalloonTipTitle = " !!!!!!!!!! ALERT !!!!!!!!!! ";
-            notifyIcon1.BalloonTipText = "A New File Was Created In [Folder Location]";
-            notifyIcon1.ShowBalloonTip(1000);       
+            FileSystemWatcher fsWatch = new FileSystemWatcher();
+            fsWatch.Path = dirPath2;
+            fsWatch.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            fsWatch.Filter = "*.*";
+            fsWatch.Created += new FileSystemEventHandler((sender, e) => onCreated(sender, e, dirPath2));
+            fsWatch.Deleted += new FileSystemEventHandler(onDeleted);
+            fsWatch.EnableRaisingEvents = true;
         }
 
+        public void watch3()
+        {
+            FileSystemWatcher fsWatch = new FileSystemWatcher();
+            fsWatch.Path = dirPath3;
+            fsWatch.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            fsWatch.Filter = "*.*";
+            fsWatch.Created += new FileSystemEventHandler((sender, e) => onCreated(sender, e, dirPath3));
+            fsWatch.Deleted += new FileSystemEventHandler(onDeleted);
+            fsWatch.EnableRaisingEvents = true;
+        }
 
+        public void onCreated(object source, FileSystemEventArgs e, string dirPathin)
+        {
+            //this causes the taskbar icon to flash until the window is focused
+            FlashWindow.Flash(this);
+
+            ballonDir = dirPathin;
+            //this is where you add label text to keep it up to date on creation
+            this.label2.Text = fileCount1(dirPath1).ToString();
+            this.label7.Text = fileCount1(dirPath2).ToString();
+
+
+            //this sets the ballootip title, text, and timer to popup when a file is created in any watched path
+            notifyIcon1.BalloonTipTitle = " !!!!!!!!!! ALERT !!!!!!!!!! ";
+            notifyIcon1.BalloonTipText = "\r A New File Was Created In \r " + dirPathin;
+            notifyIcon1.ShowBalloonTip(1000);
+        }
 
         public void onDeleted(object source, FileSystemEventArgs e)
         {
-            this.label2.Text = fileCount1().ToString();
+            //this is where you add label text to keep it up to date on deletion
+            this.label2.Text = fileCount1(dirPath1).ToString();
+            this.label7.Text = fileCount1(dirPath2).ToString();
+
+
+
         }
 
 
-        public int fileCount1()
+        public int fileCount1(string countPath)
         {
-            int count1 = Directory.GetFiles("[Folder Location]", "*", SearchOption.TopDirectoryOnly).Length;
+            var allfiles = Directory.GetFiles(countPath, "*", SearchOption.TopDirectoryOnly).Where(name => !name.EndsWith(".db"));
+            int count1 = allfiles.Count();
             return count1;
         }
 
+        private void notifyIcon1_BalloonTipClicked(object sender, EventArgs e)
+        {
+            this.Activate();
+           
+            System.Diagnostics.Process.Start("explorer.exe", ballonDir);
+           
+        }
 
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer.exe", dirPath1);
+        }
     }
 
 
