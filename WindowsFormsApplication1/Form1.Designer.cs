a6b1c96785e41bee3ac0935d0272b98f5dbb60b1
CeruleanStar <stephen.kasler@gmail.om>
2016-05-20 13:24:54 -0500

    checkboxes, borderless, colored file counts...

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -38,13 +38,17 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(47, 33);
+            this.label2.Location = new System.Drawing.Point(85, 33);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(13, 13);
             this.label2.TabIndex = 1;
@@ -53,7 +57,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 33);
+            this.label3.Location = new System.Drawing.Point(35, 33);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(26, 13);
             this.label3.TabIndex = 2;
@@ -62,7 +66,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(67, 33);
+            this.label4.Location = new System.Drawing.Point(118, 33);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(65, 13);
             this.label4.TabIndex = 3;
@@ -79,16 +83,19 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(15, 69);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Green;
+            this.label5.Location = new System.Drawing.Point(35, 58);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(136, 13);
+            this.label5.Size = new System.Drawing.Size(90, 13);
             this.label5.TabIndex = 4;
-            this.label5.Text = "Location \\\\fs3\\it\\other\\test";
+            this.label5.Text = "To Be Scheduled";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(18, 86);
+            this.label6.Location = new System.Drawing.Point(35, 86);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(26, 13);
             this.label6.TabIndex = 5;
@@ -97,7 +104,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(50, 86);
+            this.label7.Location = new System.Drawing.Point(85, 86);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(13, 13);
             this.label7.TabIndex = 6;
@@ -106,34 +113,75 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(70, 86);
+            this.label8.Location = new System.Drawing.Point(118, 86);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(65, 13);
             this.label8.TabIndex = 7;
             this.label8.Text = "Current Files";
             // 
-            // linkLabel1
+            // checkBox1
             // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
-            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabel1.ForeColor = System.Drawing.Color.Green;
-            this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.linkLabel1.Location = new System.Drawing.Point(18, 13);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(63, 13);
-            this.linkLabel1.TabIndex = 8;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Registration";
-            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(14, 9);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckStateChanged += new System.EventHandler(this.checkBox1_CheckStateChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(14, 54);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckStateChanged += new System.EventHandler(this.checkBox2_CheckStateChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(14, 110);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(15, 14);
+            this.checkBox3.TabIndex = 11;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckStateChanged += new System.EventHandler(this.checkBox3_CheckStateChanged);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(38, 110);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(48, 13);
+            this.linkLabel2.TabIndex = 12;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "IT\\Other";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(35, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Registration";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(204, 416);
-            this.Controls.Add(this.linkLabel1);
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(222, 416);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -141,8 +189,12 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Folder Tracker";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -157,7 +209,11 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -18,8 +18,15 @@ namespace WindowsFormsApplication1
     public partial class Form1 : Form
     {
         public const string dirPath1 = "\\\\tchospital\\files\\faxes\\registration";
+        public const string dirName1 = "Registration";
         public const string dirPath2 = "\\\\tchospital\\files\\faxes\\registration\\to be scheduled";
-        public const string dirPath3 = "[directory path 3]";
+        public const string dirName2 = "To Be Scheduled";
+        public const string dirPath3 = "\\\\fs3\\it\\other";
+        public const string dirName3 = "IT\\other";
+
+        public bool isbox1checked = false;
+        public bool isbox2checked = false;
+        public bool isbox3checked = false;
 
         public string ballonDir = dirPath1;
 
@@ -32,15 +39,20 @@ namespace WindowsFormsApplication1
 
             watch1();
             watch2();
-            // watch3();
-
-            this.linkLabel1.Text = dirPath1;
-            this.label5.Text = dirPath2;
+            watch3();
+            
+            
             
             // type a comment real quick there
             this.label2.Text = fileCount1(dirPath1).ToString();
+            this.label2.ForeColor = setColor(dirPath1);
             this.label7.Text = fileCount1(dirPath2).ToString();
+            this.label7.ForeColor = setColor(dirPath2);
 
+            this.FormBorderStyle = FormBorderStyle.None;
+
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            this.Location = new System.Drawing.Point(workingArea.Right - Size.Width, workingArea.Bottom - Size.Height);
         }
 
 
@@ -50,7 +62,7 @@ namespace WindowsFormsApplication1
             fsWatch.Path = dirPath1;
             fsWatch.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
             fsWatch.Filter = "*.*";
-            fsWatch.Created += new FileSystemEventHandler((sender, e) => onCreated(sender, e, dirPath1));
+            fsWatch.Created += new FileSystemEventHandler((sender, e) => onCreated(sender, e, dirPath1, isbox1checked, dirName1));
             fsWatch.Deleted += new FileSystemEventHandler(onDeleted);
             fsWatch.EnableRaisingEvents = true;
         }
@@ -61,46 +73,60 @@ namespace WindowsFormsApplication1
             fsWatch.Path = dirPath2;
             fsWatch.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
             fsWatch.Filter = "*.*";
-            fsWatch.Created += new FileSystemEventHandler((sender, e) => onCreated(sender, e, dirPath2));
+            fsWatch.Created += new FileSystemEventHandler((sender, e) => onCreated(sender, e, dirPath2, isbox2checked, dirName2));
             fsWatch.Deleted += new FileSystemEventHandler(onDeleted);
             fsWatch.EnableRaisingEvents = true;
         }
 
         public void watch3()
         {
+        
             FileSystemWatcher fsWatch = new FileSystemWatcher();
             fsWatch.Path = dirPath3;
             fsWatch.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
             fsWatch.Filter = "*.*";
-            fsWatch.Created += new FileSystemEventHandler((sender, e) => onCreated(sender, e, dirPath3));
+            fsWatch.Created += new FileSystemEventHandler((sender, e) => onCreated(sender, e, dirPath3, isbox3checked, dirName3));
             fsWatch.Deleted += new FileSystemEventHandler(onDeleted);
             fsWatch.EnableRaisingEvents = true;
+       
         }
 
-        public void onCreated(object source, FileSystemEventArgs e, string dirPathin)
+        public void onCreated(object source, FileSystemEventArgs e, string dirPathin, bool ischecked, string dirNamein)
         {
-            //this causes the taskbar icon to flash until the window is focused
-            FlashWindow.Flash(this);
+            if (ischecked)
+            {
+                //this causes the taskbar icon to flash until the window is focused
+                FlashWindow.Flash(this);
 
-            ballonDir = dirPathin;
+                ballonDir = dirPathin;
+
+
+
+                //this sets the ballootip title, text, and timer to popup when a file is created in any watched path
+                notifyIcon1.BalloonTipTitle = " !!!!!!!!!! ALERT !!!!!!!!!! ";
+                notifyIcon1.BalloonTipText = "\r A New File Was Created In \r \r " + dirNamein;
+                notifyIcon1.ShowBalloonTip(1000);
+            }
+
             //this is where you add label text to keep it up to date on creation
+            
+
             this.label2.Text = fileCount1(dirPath1).ToString();
+            this.label2.ForeColor = setColor(dirPath1);
             this.label7.Text = fileCount1(dirPath2).ToString();
+            this.label7.ForeColor = setColor(dirPath2);
 
-
-            //this sets the ballootip title, text, and timer to popup when a file is created in any watched path
-            notifyIcon1.BalloonTipTitle = " !!!!!!!!!! ALERT !!!!!!!!!! ";
-            notifyIcon1.BalloonTipText = "\r A New File Was Created In \r " + dirPathin;
-            notifyIcon1.ShowBalloonTip(1000);
         }
 
         public void onDeleted(object source, FileSystemEventArgs e)
         {
             //this is where you add label text to keep it up to date on deletion
             this.label2.Text = fileCount1(dirPath1).ToString();
+            this.label2.ForeColor = setColor(dirPath1);
             this.label7.Text = fileCount1(dirPath2).ToString();
+            this.label7.ForeColor = setColor(dirPath2);
 
-
+           
 
         }
 
@@ -114,16 +140,58 @@ namespace WindowsFormsApplication1
 
         private void notifyIcon1_BalloonTipClicked(object sender, EventArgs e)
         {
-            this.Activate();
-           
-            System.Diagnostics.Process.Start("explorer.exe", ballonDir);
-           
+            this.Activate();           
+            System.Diagnostics.Process.Start("explorer.exe", ballonDir);          
         }
 
-        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        private void checkBox1_CheckStateChanged(object sender, EventArgs e)
+        {
+            isbox1checked = !isbox1checked;
+        }
+
+        private void checkBox2_CheckStateChanged(object sender, EventArgs e)
+        {
+            isbox2checked = !isbox2checked;
+        }
+
+        private void checkBox3_CheckStateChanged(object sender, EventArgs e)
+        {
+            isbox3checked = !isbox3checked;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer.exe", dirPath2);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
             System.Diagnostics.Process.Start("explorer.exe", dirPath1);
         }
+
+        public Color setColor(string dirPathin)
+        {
+            if (fileCount1(dirPathin) == 0)
+            {
+                return Color.Green;
+            }
+            else if (fileCount1(dirPathin) > 0 && fileCount1(dirPathin) < 5)
+            {
+                return Color.Orange;
+            }
+            else if (fileCount1(dirPathin) >= 5)
+            {
+                return Color.Red;
+            }
+            else
+            {
+                return Color.HotPink;
+            }
+        }
+
+
+
+
     }
 
 
